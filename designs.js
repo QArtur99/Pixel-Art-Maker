dd299c497024e47df9566bfff2a5f84dae0f6074
ART_F <artur2113@o2.pl>
2018-03-04 17:13:46 +0100

    first commit

--- designs.js
+++ designs.js
@@ -1,10 +1,42 @@
 // Select color input
+const colorPicker = document.getElementById('colorPicker');
+colorPicker.addEventListener('click', function(event) {
+          console.log(event.target.value);
+});
+
 // Select size input
+const submit = document.getElementById('submit');
+submit.addEventListener('click', function(event) {
+  event.preventDefault();
+  let width = document.getElementById('inputWeight').value;
+  let height = document.getElementById('inputHeight').value;
+  makeGrid(width, height);
+});
 
 // When size is submitted by the user, call makeGrid()
+function makeGrid(width, height) {
+  const table = document.getElementById('pixelCanvas');
 
-function makeGrid() {
+  while (table.hasChildNodes()) {
+    table.removeChild(table.lastChild);
+  }
 
-// Your code goes here!
+  for (let h = 0; height > h; h++) {
+    const newRow = document.createElement('tr');
+    for (let w = 0; width > w; w++) {
+      const newColumnElement = document.createElement('td');
+      newColumnElement.id = 'tableElement' + h + w;
 
+      newColumnElement.addEventListener('click', function(event) {
+        event.preventDefault();
+        let color = document.getElementById('colorPicker').value;
+        event.target.style.backgroundColor = color;
+        console.log(event.target.id);
+      });
+
+      newRow.appendChild(newColumnElement);
+    }
+
+    table.appendChild(newRow);
+  }
 }
